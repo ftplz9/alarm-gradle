0a6ec5de05b54b339b9d262efad2986949dec34a
valeriygurskiy <valeriygurskiy7@gmail.com>
2022-04-01 19:42:32 +0300

    changes in main

--- src/main/java/com/alarm/Main.java
+++ src/main/java/com/alarm/Main.java
@@ -54,7 +54,7 @@ public class Main {
             long chatId = chat.id;
 
             if (chatId == -1001766138888L) {
-                System.out.println(message + " " + chatId);
+                System.out.println(message);
                 List<String> cities = Arrays.asList("м_Київ", "Вінницька_область",
                         "Дніпропетровська_область", "Рівненська_область", "Київська_область");
 
